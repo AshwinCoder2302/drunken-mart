023db0f21335a3d05781b477590ebc3475604cfd
codewithachu <codewithachu@gmail.com>
2024-09-06 18:44:31 +0530

    update environment ts

--- drunken-mart-web/src/enviroment.ts
+++ drunken-mart-web/src/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    local: true,
-    dev: false,
+    local: false,
+    dev: true,
     localURL: 'http://localhost:8082',
     devURL: 'https://drunken-mart.onrender.com'
 };
